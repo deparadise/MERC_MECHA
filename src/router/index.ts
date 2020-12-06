f3035a4ba3d75472f2d26274fc8d8d104c481890
David Pineda <david.pineda@deparadise.com>
2020-12-06 12:40:58 -0800

    plan proto routing for game demo

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,26 +1,63 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
-import Home from "../views/Home.vue";
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
+import Home from '../views/Home.vue';
+import About from '../views/About.vue';
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+	{
+		path: '/',
+		name: 'Home',
+		component: Home,
+	},
+	{
+		path: '/settings',
+		name: 'Settings',
+		component: Home,
+	},
+	{
+		path: '/about',
+		name: 'About',
+		component: About,
+	},
+	{
+		path: '/leaderboards',
+		name: 'High Scores',
+		component: () =>
+			import(/* webpackChunkName: "about" */ '../views/About.vue'),
+	},
+	{
+		path: '/store',
+		name: 'store',
+		component: () =>
+			import(/* webpackChunkName: "about" */ '../views/About.vue'),
+	},
+	{
+		path: '/game',
+		name: 'Play',
+		// route level code-splitting
+		// this generates a separate chunk (about.[hash].js) for this route
+		// which is lazy-loaded when the route is visited.
+		component: () =>
+			import(/* webpackChunkName: "about" */ '../views/About.vue'),
+		children: [
+			{
+				path: '/', // NOTE: WILL BE GAME LOBBY / PRE FLIGHT
+				component: Home,
+			},
+			{
+				path: '/:sortie', // NOTE: <<< GAME PLAY
+				component: Home,
+			},
+			{
+				path: '/:sortie/recap',
+				component: Home,
+			},
+		],
+	},
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
+	history: createWebHashHistory(),
+	routes,
 });
 
 export default router;
