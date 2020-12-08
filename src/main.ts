0d08a66f8b9fe3d3acbb49aca3a6dc61800da024
David Pineda <david.pineda@deparadise.com>
2020-12-07 22:07:36 -0800

    yarn add vue-material and plan for integration once V3 is supported

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import './registerServiceWorker';
 import router from './router';
 import store from './store';
 
+//TODO: Can't use vue-material untill V3 is supported
+// https://github.com/vuematerial/vue-material/issues/2277
+// import VueMaterial from 'vue-material';
+
 createApp(App)
 	.use(store)
 	.use(router)
+	// .use(VueMaterial)
 	.mount('#app');
--- src/views/Game/Hangar.vue
+++ src/views/Game/Hangar.vue
@@ -2,6 +2,7 @@
 	<div>
 		<h1>This will be the Pre Game Hangar Mech Mod Page</h1>
 	</div>
+	<p class="vue-material">VUE MATERIAL HERE</p>
 </template>
 
 <script>
