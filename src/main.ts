e3e4ce451570f47e7a363e9c39052ef1ef574e72
David Pineda <david.pineda@deparadise.com>
2020-12-07 22:27:37 -0800

    vue add bootstrap-vue and plan for integration once V3 is supported

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,18 @@ import './registerServiceWorker';
 import router from './router';
 import store from './store';
 
+// NOTE: VUE3 UNSUPPORTED
+// https://github.com/bootstrap-vue/bootstrap-vue/issues/5196#issuecomment-700426488
+import '@babel/polyfill';
+import 'mutationobserver-shim';
+import './plugins/bootstrap-vue';
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
 createApp(App)
 	.use(store)
 	.use(router)
+	.use(BootstrapVue)
+	.use(BootstrapVueIcons)
 	.mount('#app');
